3bd5a12a9f25fb946a388301ffb628f04238d43c
Hassan Tarek Abu Helal <hassantarek201517@gmail.com>
2020-11-23 21:18:26 +0200

    list2 of shops

---
+++ app/src/main/java/info/androidhive/loginandregistration/activity/CustomListView2.java
@@ -0,0 +1,78 @@
+package info.androidhive.loginandregistration.activity;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import info.androidhive.loginandregistration.R;
+
+/**
+ * Created by jaiso on 13-02-2018.
+ */
+
+public class CustomListView2 extends ArrayAdapter<String> {
+
+    private String[] shopName;
+    private String[] price;
+    private String[] specialOffer;
+    private Activity context;
+    Bitmap bitmap;
+
+    public CustomListView2(Activity context, String[] shopName, String[] price, String[] specialOffer) {
+        super(context, R.layout.layout2, shopName);
+        this.context=context;
+        this.shopName = shopName;
+        this.price = price;
+        this.specialOffer = specialOffer;
+    }
+
+    @NonNull
+    @Override
+
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View r=convertView;
+        ViewHolder viewHolder=null;
+        if(r==null){
+            LayoutInflater layoutInflater=context.getLayoutInflater();
+            r=layoutInflater.inflate(R.layout.layout2,null,true);
+            viewHolder=new ViewHolder(r);
+            r.setTag(viewHolder);
+        }
+        else {
+            viewHolder=(ViewHolder)r.getTag();
+
+        }
+
+        viewHolder.tvw1.setText(shopName[position]);
+        viewHolder.tvw2.setText(price[position]);
+        viewHolder.tvw3.setText(specialOffer[position]);
+
+
+        return r;
+    }
+
+    class ViewHolder{
+
+        TextView tvw1;
+        TextView tvw2;
+        TextView tvw3;
+
+        ViewHolder(View v){
+            tvw1=(TextView)v.findViewById(R.id.shopname);
+            tvw2=(TextView)v.findViewById(R.id.price);
+            tvw3=(TextView)v.findViewById(R.id.offer);
+        }
+
+    }
+
+
+
+
+}
--- app/src/main/java/info/androidhive/loginandregistration/activity/Shop.java
+++ app/src/main/java/info/androidhive/loginandregistration/activity/Shop.java
@@ -20,9 +20,16 @@ import info.androidhive.loginandregistration.R;
 public class Shop extends AppCompatActivity {
 
     String urladdress = "http://172.21.16.1/android_login_api/display_shop_product.php";
-    String[] shopName;
+    String urladdress2 = "http://172.21.16.1/android_login_api/display_shop.php";
+    String[] shopID;
+    String[] productID;
     String[] price;
     String[] specialOffer;
+
+    String[] shopName;
+    String[] latitude;
+    String[] longitude;
+
     ListView listView;
     BufferedInputStream is;
     String line = null;
@@ -37,24 +44,24 @@ public class Shop extends AppCompatActivity {
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if(extras == null) {
-                id= 0;
+                id = 0;
             } else {
-                id= extras.getInt("product_ID");
+                id = extras.getInt("product_ID")+1;
             }
         } else {
-            id= (int) savedInstanceState.getSerializable("product_ID");
+            id = (int) savedInstanceState.getSerializable("product_ID")+1;
         }
 
         listView = (ListView) findViewById(R.id.lview2);
 
         StrictMode.setThreadPolicy((new StrictMode.ThreadPolicy.Builder().permitNetwork().build()));
-        collectData();
-        //CustomListView customListView = new CustomListView(this, shopName, price, specialOffer);
-        //listView.setAdapter(customListView);
+        collectData1();
+        CustomListView2 customListView = new CustomListView2(this, shopName, price, specialOffer);
+        listView.setAdapter(customListView);
     }
 
 
-    private void collectData() {
+    private void collectData1() {
 //Connection
         try {
 
@@ -85,16 +92,84 @@ public class Shop extends AppCompatActivity {
         try {
             JSONArray ja = new JSONArray(result);
             JSONObject jo = null;
-            shopName = new String[ja.length()];
+            productID = new String[ja.length()];
+            shopID = new String[ja.length()];
             price = new String[ja.length()];
             specialOffer = new String[ja.length()];
-
+            int k=0;
             for (int i = 0; i <= ja.length(); i++) {
                 jo = ja.getJSONObject(i);
-                shopName[i] = jo.getString("name");
-                price[i] = jo.getString("description");
-                specialOffer[i] = jo.getString("image_url");
+                productID[i] = jo.getString("product_id");
+                int idtmp = Integer.parseInt(productID[i]);
+                if (idtmp==id){
+                    shopID[k] = jo.getString("shop_id");
+                    price[k] = jo.getString("price");
+                    specialOffer[k] = jo.getString("available_special_offers");
+                    k++;
+                }
+
             }
+
+
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+        collectData2(shopID);
+
+
+    }
+
+    private void collectData2(String[] shopID) {
+//Connection
+        try {
+
+            URL url = new URL(urladdress2);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            is = new BufferedInputStream(con.getInputStream());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        //content
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+
+//JSON
+        try {
+            JSONArray ja = new JSONArray(result);
+            JSONObject jo = null;
+            shopName = new String[ja.length()];
+            latitude = new String[ja.length()];
+            longitude = new String[ja.length()];
+            int k=0;
+            for (int i = 0; i <= ja.length(); i++) {
+                jo = ja.getJSONObject(i);
+                String idtmp = jo.getString("id");
+                for (int j = 0; j <shopID.length ; j++) {
+                    if(idtmp.equals(shopID[j])){
+                        shopName[k] = jo.getString("name");
+                        latitude[k] = jo.getString("latitude");
+                        longitude[k] = jo.getString("longitude");
+                        k++;
+                    }
+                }
+            }
+
+
         } catch (Exception ex) {
 
             ex.printStackTrace();
